43493a6e5d30a3b3fe13b718c8cf9f53cfe96b0a
jungho.baek <jungho.baek@42dot.ai>
2023-04-16 19:24:48 +0900

    Configuration MySQL docker and Entities (#3)

--- api/src/main/kotlin/team/backend/infrastructure/BaseEntity.kt
+++ api/src/main/kotlin/team/backend/infrastructure/BaseEntity.kt
@@ -1,12 +1,12 @@
 package team.backend.infrastructure
 
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
-import javax.persistence.EntityListeners
-import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 @EntityListeners
