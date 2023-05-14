3a1afa2e73bc17b0cb229c38d46981e3ea9f7e82
FLex <qjxjfld13@gmail.com>
2023-05-14 15:32:43 +0900

    refactor: Dependency management and Gradle Optimization (#24)

---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,10 @@
+object Plugins {
+    const val SPRING_BOOT = "org.springframework.boot"
+    const val SPRING_DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
+    const val JVM = "jvm"
+    const val SPRING = "plugin.spring"
+    const val JPA = "plugin.jpa"
+    const val JETBRAINS_KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
+    const val JETBRAINS_KOTLIN_SPRING = "org.jetbrains.kotlin.plugin.spring"
+    const val JETBRAINS_KOTLIN_JPA = "org.jetbrains.kotlin.plugin.jpa"
+}
---
+++ buildSrc/src/main/kotlin/Utils.kt
@@ -0,0 +1,19 @@
+object Projects {
+    const val GROUP = "team.backend"
+    const val VERSIONS = "0.0.1-SNAPSHOT"
+}
+
+object Modules {
+    const val CORE = ":core"
+    const val API = ":api"
+    const val CLIENT = ":client"
+}
+
+object CompilerOptions {
+    const val NULL_SAFETY = "-Xjsr305=strict"
+}
+
+object Resources {
+    const val SOURCE_PATH = "../chaterview-private"
+    const val DESTINATION_PATH = "./src/main/resources"
+}
